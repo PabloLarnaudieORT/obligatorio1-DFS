168bda7792b9a827e3cbf8dd2dc4ff22c96674bb
pablo <pablolarnaudie29@gmail.com>
2026-06-02 11:05:45 -0300

    removiendo circulo de dependencias en validators

--- v1/validators/desafios.validators.js
+++ v1/validators/desafios.validators.js
@@ -2,16 +2,15 @@ import Joi from "joi";
 
 export const editarDesafiosSchema = Joi.object({
   nombreDesafio: Joi.string().min(2).max(100).optional(),
-  fechaCreacion: Joi.date().max(Joi.ref('fechaLimite')).optional(),
-  fechaLimite: Joi.date().min(Joi.ref('fechaCreacion')).optional(),
+  fechaLimite: Joi.date().optional(),
   puntosDesafio: Joi.number().min(0).max(10000).optional(),
   categoriaZonaMuscular: Joi.string().optional(),
 });
 
 export const crearDesafiosSchema = Joi.object({
   nombreDesafio: Joi.string().min(2).max(100).required(),
-  fechaCreacion: Joi.date().max(Joi.ref('fechaLimite')).required(),
-  fechaLimite: Joi.date().min(Joi.ref('fechaCreacion')).required(),
+  fechaCreacion: Joi.date().required(),
+  fechaLimite: Joi.date().min(Joi.ref("fechaCreacion")).required(),
   puntosDesafio: Joi.number().min(0).max(10000).required(),
   categoriaZonaMuscular: Joi.string().required(),
 });
