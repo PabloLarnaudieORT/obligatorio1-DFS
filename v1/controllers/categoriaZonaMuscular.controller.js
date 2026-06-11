63b6a4421da53f53b5b8681306aec53bb23d4124
pablo <pablolarnaudie29@gmail.com>
2026-06-11 00:13:19 -0300

    cambios de nati

--- v1/controllers/categoriaZonaMuscular.controller.js
+++ v1/controllers/categoriaZonaMuscular.controller.js
@@ -1,19 +1,25 @@
 import {
-    crearCategoriaZonaMusculoService,
-    obtenerCategoriaZonaMusculoService,
-    obtenerCategoriaZonaMusculoServicePorIdService,
-    actualizarCategoriaZonaMusculoService,
-    eliminarCategoriaZonaMusculoService
+  crearCategoriaZonaMusculoService,
+  obtenerCategoriaZonaMusculoService,
+  obtenerCategoriaZonaMusculoServicePorIdService,
+  actualizarCategoriaZonaMusculoService,
+  eliminarCategoriaZonaMusculoService,
 } from "../services/categoriaZonaMuscular.services.js";
 
 export const crearCategoriaZonaMusculo = async (req, res) => {
-    const userId = req.user?.id;
-    const categoriaCreada = await crearCategoriaZonaMusculoService(req.body, userId);
-    res.json({ message: "Categoría de zona muscular agregada exitosamente", ...categoriaCreada });
-}
+  const userId = req.user?.id;
+  const categoriaCreada = await crearCategoriaZonaMusculoService(
+    req.body,
+    userId,
+  );
+  res.json({
+    message: "Categoría de zona muscular agregada exitosamente",
+    ...categoriaCreada,
+  });
+};
 
 export const obtenerCategoriaZonaMusculo = async (req, res) => {
-    //nosotros decidimos si filtramos por userid o por rol
+  //nosotros decidimos si filtramos por userid o por rol
   const userId = req.user?.id;
   const userRol = req.user?.rol;
   const userIdFilter = userRol === "admin" ? null : userId;
@@ -25,34 +31,56 @@ export const obtenerCategoriaZonaMusculo = async (req, res) => {
     message: "Categorías de zona muscular obtenidas",
     categorias: categoriasObtenidas,
   });
-}
+
+  /*
+    const categoriasObtenidas = await obtenerCategoriaZonaMusculoService();
+    res.json({ message: "Categorías de zona muscular obtenidas", categorias: categoriasObtenidas });
+    */
+};
 
 export const obtenerCategoriaZonaMusculoServicePorId = async (req, res) => {
-    const { id } = req.params;
-    const userId = req.user?.id;
-    const userRol = req.user?.rol;
-    const userIdFilter = userRol === 'admin' ? null : userId;
-    
-    const categoriaObtenida = await obtenerCategoriaZonaMusculoServicePorIdService(id, userIdFilter);
-    res.json({ message: `Categoría de zona muscular ${categoriaObtenida.id} obtenida con exito`, categoria: categoriaObtenida });
-}
+  const { id } = req.params;
+  const userId = req.user?.id;
+  const userRol = req.user?.rol;
+  const userIdFilter = userRol === "admin" ? null : userId;
+
+  const categoriaObtenida =
+    await obtenerCategoriaZonaMusculoServicePorIdService(id, userIdFilter);
+  res.json({
+    message: `Categoría de zona muscular ${categoriaObtenida.id} obtenida con exito`,
+    categoria: categoriaObtenida,
+  });
+};
 
 export const actualizarCategoriaZonaMusculo = async (req, res) => {
-    const { id } = req.params;
-    const userId = req.user?.id;
-    const userRol = req.user?.rol;
-    const userIdFilter = userRol === 'admin' ? null : userId;
-    
-    const categoriaActualizada = await actualizarCategoriaZonaMusculoService(id, req.body, userIdFilter);
-    res.json({ message: `Categoría de zona muscular ${categoriaActualizada.id} actualizada exitosamente`, ...categoriaActualizada });
-}
+  const { id } = req.params;
+  const userId = req.user?.id;
+  const userRol = req.user?.rol;
+  const userIdFilter = userRol === "admin" ? null : userId;
+
+  const categoriaActualizada = await actualizarCategoriaZonaMusculoService(
+    id,
+    req.body,
+    userIdFilter,
+  );
+  res.json({
+    message: `Categoría de zona muscular ${categoriaActualizada.id} actualizada exitosamente`,
+    ...categoriaActualizada,
+  });
+};
 
 export const eliminarCategoriaZonaMusculo = async (req, res) => {
-    const { id } = req.params;
-    const userId = req.user?.id;
-    const userRol = req.user?.rol;
-    const userIdFilter = userRol === 'admin' ? null : userId;
-    
-    const categoriaEliminada = await eliminarCategoriaZonaMusculoService(id, userIdFilter);
-    res.json({ message: `Categoría de zona muscular ${categoriaEliminada.id} eliminada exitosamente`, ...categoriaEliminada });
-}
+  const { id } = req.params;
+  const userId = req.user?.id;
+  const userRol = req.user?.rol;
+  const userIdFilter = userRol === "admin" ? null : userId;
+
+  const categoriaEliminada = await eliminarCategoriaZonaMusculoService(
+    id,
+    userIdFilter,
+  );
+  res.json({
+    message: `Categoría de zona muscular ${categoriaEliminada.id} eliminada exitosamente`,
+    ...categoriaEliminada,
+  });
+};
